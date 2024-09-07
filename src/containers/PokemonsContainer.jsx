a36e27c0ec1f68e61a0cd9f6189196f5dd98949c
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2024-09-07 15:46:06 -0600

    updates on styling and resposiveness

--- src/containers/PokemonsContainer.jsx
+++ src/containers/PokemonsContainer.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { Pokemon } from '../components/Pokemon.jsx'
+import { GET_POKEMONS } from '../graphql/get-pokemons.jsx'
+
 
 export function PokemonsContainer() {
-    return (
-        <p>I am a pokemon container</p>
-    )
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { first: 10 },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const { pokemons = [] } = data;
+
+  return (
+    <div className="container">
+      {pokemons.map(pokemon => (
+        <Pokemon key={pokemon.id} pokemon={pokemon} />
+      ))}
+    </div>
+  );
 }
 
-/*export default App */
+
+
+
+/*
+
+export function PokemonsContainer() {
+   const { data: { pokemons = [] } = {} } = useQuery(pokemons, {
+      variables: { first: 9 },
+   });
+      return (
+        <div className="conatiner">
+          {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}          
+        </div>
+    )
+}
+*/
